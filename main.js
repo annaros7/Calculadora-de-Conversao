6807a9bf82bd4cd15c8f00bd7b813873c1a1fef4
Anna Rossete <124687030+annaros7@users.noreply.github.com>
2024-12-10 10:24:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 function calcular() {
-    var v1 = document.getElementById('valor1');
-    var v2 = document.getElementById('valor2');
-    var gerar = document.getElementById('gerador');
-    var res = document.getElementById('res');
-    var n1 = Number(v1.value);
-    var n2 = Number(v2.value);
+    let v1 = document.getElementById('valor1');
+    let v2 = document.getElementById('valor2');
+    let gerar = document.getElementById('gerador');
+    let res = document.getElementById('res');
+    let n1 = Number(v1.value);
+    let n2 = Number(v2.value);
 
-    var varFinal = (n1*1000)/n2;
+    let varFinal = (n1*1000)/n2;
 
     let varFinalArred = varFinal.toFixed(2)
 
-    res.innerHTML = varFinalArred
+    res.innerHTML = varFinalArred;
+}
+
+function copyText() {
+    let copyArea = document.getElementById('res').innerHTML;
+    navigator.clipboard.writeText(copyArea);
 }
